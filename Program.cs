44cf5bcf767b34fad930a2bbc4401bb7c03b49cd
Robert Tublén <robert.tublen@gmail.com>
2020-08-31 11:28:43 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using static System.Console;
+
+namespace SocialSecurityNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Social Security Number (YYMMDD-XXXX): ");
+
+            string socialSecurityNumber = ReadLine();
+            string gender;
+
+            int genderNumber = int.Parse(socialSecurityNumber.Substring(socialSecurityNumber.Length - 2, 1));
+
+            bool isFemale = genderNumber % 2 == 0;
+
+            gender = isFemale ? "Female" : "Male";
+
+            DateTime birthDate = DateTime.ParseExact(socialSecurityNumber.Substring(0, 6), "yyMMdd", CultureInfo.InvariantCulture);
+
+            int age = DateTime.Now.Year - birthDate.Year;
+
+            if ((birthDate.Month > DateTime.Now.Month) || (birthDate.Month == DateTime.Now.Month && birthDate.Day > DateTime.Now.Day))
+            {
+                age--;
+            }
+
+            WriteLine($"{gender}, {age}");
+        }
+    }
+}
